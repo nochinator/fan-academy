c3f96cd0c8cbd3441f24524db59366f333843e3e
Dan-DH <daniel.diaz.hdez@gmail.com>
2025-07-04 14:27:57 +0200

    fix: display on mobile

--- src/main.ts
+++ src/main.ts
@@ -7,15 +7,14 @@ import ProfileScene from './scenes/profile.scene';
 import PreloaderScene from './scenes/preloader.scene';
 
 const config = {
-  width: window.innerWidth,
-  height: window.innerHeight,
   backgroundColor: '#808080',
-  type: Phaser.AUTO, // webgl is browser supports it, fallback to canvas
+  type: Phaser.AUTO, // webgl if browser supports it, fallback to canvas
   scene: [PreloaderScene, MainMenuScene, UIScene, GameScene, LeaderboardScene, ProfileScene],
   scale: {
-    mode: Phaser.Scale.FIT, // Automatically resize the canvas on window resize
-    autoCenter: Phaser.Scale.CENTER_BOTH, // Center the canvas
-    orientation: Phaser.Scale.LANDSCAPE
+    mode: Phaser.Scale.FIT, // automatically resize the canvas on window resize
+    autoCenter: Phaser.Scale.CENTER_BOTH, // center the canvas
+    width: 1434,
+    height: 990
   },
   dom: { createContainer: true },
   parent: 'app'
