9e3a920c5774e8f817365fb013ea690bf34d72ea
Dan-DH <daniel.diaz.hdez@gmail.com>
2025-07-17 11:46:24 +0200

    chore: removed phaser cdn script

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,6 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   build: {
     rollupOptions: {
-      external: ['phaser'],
-      output: { globals: { phaser: 'Phaser' } },
       plugins: [
         visualizer({
           filename: 'dist/stats.html',
