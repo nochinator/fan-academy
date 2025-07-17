3feeb6e98739a163710d24778e94ec82b255dc64
Dan-DH <daniel.diaz.hdez@gmail.com>
2025-07-17 11:53:57 +0200

    chore: switched to phaser-arcade-physics

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,26 @@
+import path from 'path';
 import { visualizer } from 'rollup-plugin-visualizer';
 import { defineConfig } from 'vite';
+
+import { dependencies } from './package.json';
+function renderChunks(deps: Record<string, string>) {
+  const chunks = {};
+  Object.keys(deps).forEach((key) => {
+    if (['phaser'].includes(key)) return;
+    chunks[key] = [key];
+  });
+  return chunks;
+}
 
 export default defineConfig({
   build: {
     rollupOptions: {
+      output: {
+        manualChunks: {
+          phaser: ['phaser'],
+          ...renderChunks(dependencies)
+        }
+      },
       plugins: [
         visualizer({
           filename: 'dist/stats.html',
@@ -13,5 +30,6 @@ export default defineConfig({
         })
       ]
     }
-  }
+  },
+  resolve: { alias: { phaser: path.resolve(__dirname, 'node_modules/phaser/dist/phaser-arcade-physics.min.js') } }
 });
