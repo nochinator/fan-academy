b75a89436dbd93f78403d6776da61e64040dc811
Dan-DH <daniel.diaz.hdez@gmail.com>
2025-08-07 10:52:13 +0200

    fix: corrected voidmonk image when a soulstone and helmet are applied

--- src/scenes/preloader.scene.ts
+++ src/scenes/preloader.scene.ts
@@ -1,4 +1,4 @@
-export const CDN_PATH = 'https://cdn.jsdelivr.net/gh/Dan-DH/fa-assets@0979120';
+export const CDN_PATH = 'https://cdn.jsdelivr.net/gh/Dan-DH/fa-assets@f85ebf2';
 
 // PreloaderScene.ts
 export default class PreloaderScene extends Phaser.Scene {
