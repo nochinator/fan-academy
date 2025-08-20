93c7fa91c62f4e8c598eb272a9097c3325866ea2
Dan-DH <daniel.diaz.hdez@gmail.com>
2025-08-20 13:41:29 +0200

    fix: necro losing attack modifiers before damage calculation

--- src/classes/elves.ts
+++ src/classes/elves.ts
@@ -230,12 +230,11 @@ export class Necromancer extends DarkElf {
     } else {
       if (this.superCharge) playSound(this.scene, EGameSounds.NECROMANCER_ATTACK_BIG);
       if (!this.superCharge) playSound(this.scene, EGameSounds.NECROMANCER_ATTACK);
+
+      const damageDone = target.getsDamaged(this.getTotalPower(), this.attackType);
+      if (damageDone) this.lifeSteal(damageDone);
       this.removeAttackModifiers();
     }
-
-    const damageDone = target.getsDamaged(this.getTotalPower(), this.attackType);
-
-    if (damageDone) this.lifeSteal(damageDone);
 
     if (target && target instanceof Hero && target.isKO && target.unitType === EHeroes.PHANTOM) target.removeFromGame();
     this.context.gameController!.afterAction(EActionType.ATTACK, this.boardPosition, target.boardPosition);
